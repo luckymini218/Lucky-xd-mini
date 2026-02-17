d0320ce4423be413f904dbc9dce49b947d817043
luckymini218 <luckyxdmini@gmail.com>
2026-02-17 17:56:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     AUTO_RECORDING: 'true',
     AUTO_LIKE_EMOJI: ['🌸', '🪴', '💫', '🍂', '🌟','🫀', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'],
     PREFIX: '.',
-    MAX_RETRIES: 3,
+    MAX_RETRIES: 30,
     GROUP_INVITE_LINK: 'https://chat.whatsapp.com/Lixf2Pcp9fd0wjL4mv2WHJ',
     ADMIN_LIST_PATH: './admin.json',
     IMAGE_PATH: 'https://files.catbox.moe/f9gwsx.jpg',
